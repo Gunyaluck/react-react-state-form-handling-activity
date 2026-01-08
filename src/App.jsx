279b757a07f30d5c1d66b80f92a9717c0745cddb
MOGunyaluck <gunyaluck0619@gmail.com>
2026-01-08 10:53:57 +0700

    Implement input handling and display functionality in App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,28 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
+  //มี 2 event คือ input และ button ใช้ useState 2 ตัวเพื่อจัดการข้อมูล
+  const [inputValue, setInputValue] = useState("");
+  const [displayValue, setDisplayValue] = useState("");
+
+  function handleAdd() {
+    setDisplayValue(inputValue);
+  }
+
+  function handleInputChange(event) {
+    setInputValue(event.target.value);
+  }
+
   return (
     <div className="App">
       <h1>Simple Greeting Message App 2</h1>
       <div className="greeting-container">
-        <h2>ผลลัพธ์จะมาแทนที่ข้อความนี้</h2>
-        <input type="text" />
+        <h2>{displayValue}</h2>
+        <input type="text" value={inputValue} onChange={handleInputChange} />
+        {/*หรือใช้ onChange = {(e) => setInputValue(e.target.value)}*/}
         <div className="buttons">
-          <button>Add</button>
+          <button onClick={handleAdd}>Add</button>
+          {/*หรือใช้ onClick = {() => setDisplayValue(inputValue)}*/}
         </div>
       </div>
     </div>
