c5295d759223ed0a770925addb0fe434f79af9a9
MOGunyaluck <gunyaluck0619@gmail.com>
2026-01-08 10:10:50 +0700

    feat: implement dynamic greeting functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,31 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [greeting, setGreeting] = useState("");
+
+  const handleGreeting = (greeting) => {
+    setGreeting(greeting);
+  };
+
+  // //อีกวิธีหนึ่ง
+  // function handleClickThaiGreeting () {
+  //   setGreeting("สวัสดี");
+  // }
+
+  // function handleClickEnglishGreeting () {
+  //   setGreeting("Hi");
+  // }
+
   return (
     <div className="App">
       <h1>Simple Greeting Message App</h1>
       <div className="greeting-container">
-        <h2>ผลลัพธ์จะมาแทนที่ข้อความนี้</h2>
+        <h2>{greeting}</h2>
         <div className="buttons">
-          <button>Hi</button>
-          <button>สวัสดี</button>
+          <button onClick={() => handleGreeting("Hi")}>Hi</button>
+          <button onClick={() => handleGreeting("สวัสดี")}>สวัสดี</button>
+          {/* <button onClick={handleClickThaiGreeting}>สวัสดี</button>
+          <button onClick={handleClickEnglishGreeting}>Hi</button> */}
         </div>
       </div>
     </div>
